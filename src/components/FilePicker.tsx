d9775d71d38f3ab110687fed090eaccbde8c8b8f
Jake Prins <jakeprins@outlook.com>
2021-09-13 11:46:21 +0200

    Add trigger prop to FilePicker

--- src/components/FilePicker.tsx
+++ src/components/FilePicker.tsx
@@ -1,25 +1,26 @@
-import React, { FC, HTMLAttributes, useState } from 'react'
+import React, { FC, Fragment, HTMLAttributes, ReactElement, useState } from 'react'
 
 import { Modal } from './Modal'
 import { ModalContent } from './ModalContent'
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** The JSON Web Token returned from the Session call */
+  // The component that should trigger the File Picker modal on click
+  trigger: ReactElement
+  // The JSON Web Token returned from the Session call
   jwt: string
 }
 
 /**
  * The Apideck File Picker
  */
-export const FilePicker: FC<Props> = ({ jwt }) => {
+export const FilePicker: FC<Props> = ({ trigger, jwt }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
-
   return (
-    <div>
-      <button onClick={() => setIsOpen(true)}>Pick file</button>
+    <Fragment>
+      {React.cloneElement(trigger, { onClick: () => setIsOpen(true) })}
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <ModalContent jwt={jwt} />
       </Modal>
-    </div>
+    </Fragment>
   )
 }
