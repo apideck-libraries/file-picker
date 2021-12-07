2937244b36c8d7cdf35f69947f1f387333fb6e75
Jake Prins <jakeprins@outlook.com>
2021-12-07 11:21:45 +0100

    Use env vars in stories

--- stories/FilePicker.stories.tsx
+++ stories/FilePicker.stories.tsx
@@ -21,14 +21,13 @@ const meta: Meta = {
 export default meta
 
 // First create a vault session to get a JSON Web Token
-const jwt =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWRpcmVjdF91cmkiOiJodHRwczovL215c2Fhcy5jb20vZGFzaGJvYXJkIiwiY29uc3VtZXJfbWV0YWRhdGEiOnsiYWNjb3VudF9uYW1lIjoiU3BhY2VYIiwidXNlcl9uYW1lIjoiRWxvbiBNdXNrIiwiaW1hZ2UiOiJodHRwczovL3d3dy5zcGFjZXguY29tL3N0YXRpYy9pbWFnZXMvc2hhcmUuanBnIn0sInRoZW1lIjp7InZhdWx0X25hbWUiOiJJbnRlcmNvbSIsInByaW1hcnlfY29sb3IiOiIjMjg2ZWZhIiwic2lkZXBhbmVsX2JhY2tncm91bmRfY29sb3IiOiIjMjg2ZWZhIiwic2lkZXBhbmVsX3RleHRfY29sb3IiOiIjRkZGRkZGIiwiZmF2aWNvbiI6Imh0dHBzOi8vd3d3LmludGVyY29tLmNvbS9fbmV4dC9zdGF0aWMvaW1hZ2VzL2Zhdmljb24tMzQ4YWQ4ZGY2YzE2N2Q5M2ZiODk0ZThiY2I0ZGNkMmEucG5nIiwidGVybXNfdXJsIjoiaHR0cHM6Ly93d3cudGVybXNmZWVkLmNvbS90ZXJtcy1jb25kaXRpb25zLzk1N2M4NWMxYjA4OWFlOWUzMjE5YzgzZWZmNjUzNzdlIiwicHJpdmFjeV91cmwiOiJodHRwczovL2NvbXBsaWFuY2UuYXBpZGVjay5jb20vcHJpdmFjeS1wb2xpY3kifSwiY29uc3VtZXJfaWQiOiJ0ZXN0LWNvbnN1bWVyLWNrZ3JzOTVsM3k0ZXIwYjk5cWEzN2J1ajIiLCJhcHBsaWNhdGlvbl9pZCI6ImNmYVpyT1JnYUgyUE1RcEljalRwZmhFUklwSUVVSkhldjA5dWNqVHAiLCJzY29wZXMiOltdLCJpYXQiOjE2Mzg3OTc3MzAsImV4cCI6MTYzODgwMTMzMH0.Ea-7etB3NH1gao3bLVKs85G0e1AlD2Z9Z5cjgzm5cPg'
+const jwt = 'xxxxxx'
 
 const Template: Story<Props> = (args) => (
   <FilePicker
     trigger={<button className="p-2 border rounded shadow">Pick a file</button>}
-    appId="cfaZrORgaH2PMQpIcjTpfhERIpIEUJHev09ucjTp"
-    consumerId={'test-consumer-ckgrs95l3y4er0b99qa37buj2'}
+    appId={process.env.STORYBOOK_APP_ID}
+    consumerId={process.env.STORYBOOK_CONSUMER_ID}
     jwt={jwt}
     onSelect={(file) => console.log(file)}
     {...args}
